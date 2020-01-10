a4f8f53517b9c7c676267c0bdd07029353e2c111
KikeyMeenan <mikemk309@googlemail.com>
2020-01-10 09:33:08 +0000

    initial commit

---
+++ AddressBook.Tests.Unit/AddressBook.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace AddressBook.Tests.Unit
+{
+    public class AddressBook
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
